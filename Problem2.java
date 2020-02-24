40bea6f7d30aaaecce61286eff1033e629ed3a95
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-02-24 15:04:08 -0800

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,28 @@
-
+import java.io.*;
+import java.util.Scanner; 
+  
+public class Problem2 { 
+  
+		public static boolean isPowerOfTwo(int n) {
+		    if(n<=0) 
+		        return false;
+		 
+		    while(n>2){
+		        int t = n>>1;
+		        int c = t<<1;
+		 
+		        if(n-c != 0)
+		            return false;
+		 
+		        n = n>>1;
+		    }
+		 
+		    return true;
+		}
+public static void main(String[] args) {
+	Scanner console = new Scanner(System.in);
+	int m = console.nextInt();
+	System.out.println(isPowerOfTwo(m));
+	
+}
+}
