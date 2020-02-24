3fa8307a0c13a63145bfe1d06d47c5f7d81ab4bf
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-02-24 14:35:43 -0800

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,20 +1,19 @@
-//
 import java.util.*;
 
 public class Problem1 {
     public static void main(String[] args) {
     	int count = 1;
     	for (int i = 1; i <= 6; i++ ) {
-        int number = 1;
+    		int number = 1;
         
-        Random random = new Random();
-        int randomvalue = random.nextInt(2);
+    		Random random = new Random();
+    		int randomvalue = random.nextInt(2);
         
 
-        while (randomvalue != number) {
-            randomvalue = random.nextInt(2);
-            count++;
-        }
+    		while (randomvalue != number) {
+    			randomvalue = random.nextInt(2);
+    			count++;
+    		}
     	}
         
         System.out.println("number of falls: " + 
